01464e0580b0306310f1959f79d176870fb159d4
Reda Rahmoune <61332582+Zeghtlafou@users.noreply.github.com>
2022-01-25 20:59:57 -0500

    Add files via upload

---
+++ seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO Liverpool_pLayer
+  (id, Joueur, Annee_debut,Annee_fin )
+VALUES
+  (1, 'Steven Gerrard', 1998 , 2015),
+  (2, 'Luis Suarez', 2011, 2014),
+  (3, 'Mohamed Salah', 2017, 2023),
+  (4, 'Virgil Van Djik', 2019, 2025);
+  
+
