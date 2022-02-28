b223e79a5628f7f8d4551a65f5b9981bd51fdbc9
Reda Rahmoune <61332582+Zeghtlafou@users.noreply.github.com>
2022-02-28 11:43:49 -0500

    Add files via upload

---
+++ seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO teams (player_name,season,coach)
+VALUES('van-djik','W-2020','kloop'),
+      ('salah','W-2020','kloop'),
+      ('mane','W-2020','kloop'),
+      ('henderson','W-2020','kloop');
+	  
+	  
+ALTER TABLE teams
+ADD id integer;
---
+++ shema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE teams(
+player_name varchar(255);
+season varchar(255);
+coach varchar(255),
+id varchar(255),
+PRIMARY KEY (id)
+
+);
